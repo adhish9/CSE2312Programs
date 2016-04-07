7ca46acfdac01f6ab470b2a8592ae46a207d4b14
adhish9 <adhish.deshpande@gmail.com>
2016-04-06 21:26:37 -0500

    Update Program1.s

--- Program1.s
+++ Program1.s
@@ -3,28 +3,28 @@
 @1001104148
 
 
-.global main
+    .global main
     .func main
 
 main:
-    BL  _prompt1             @ branch to prompt procedure with return
-    BL  _scanf1             @ branch to scanf procedure with return
-    MOV R4, R0              @ move return value R0 to argument register R1
+    BL  _prompt1            @ branch to prompt1 procedure with return
+    BL  _scanf1             @ branch to scanf1 procedure with return
+    MOV R4, R0              @ move return value R0 to argument register R4
 
-    BL  _prompt2         @ branch to printf procedure with return
-    BL  _scanf2            @ brach to getchar procedure with return
-    MOV R5, R0              @ move return value R0 to argument register R3
+    BL  _prompt2            @ branch to prompt2 procedure with return
+    BL  _scanf2             @ brach to scanf2 procedure with return
+    MOV R5, R0              @ move return value R0 to argument register R5
 
-    BL  _prompt3             @ branch to prompt procedure with return
-    BL  _scanf3              @ branch to scanf procedure with return
-    MOV R6, R0              @ move return value R0 to argument register R1
+    BL  _prompt3            @ branch to prompt3 procedure with return
+    BL  _scanf3             @ branch to scanf3 procedure with return
+    MOV R6, R0              @ move return value R0 to argument register R6
 
     MOV R1, R4
     MOV R2, R5
     MOV R3, R6
-    BL  _comp            @ branch to compare procedure
+    BL  _comp               @ branch to compare procedure
     MOV R1, R0
-    BL  _ans        @ branch to printf procedure with return
+    BL  _ans                @ branch to answer procedure with return
 
     B   main                @ branch to main procedure with no return
 
@@ -33,7 +33,7 @@ _prompt1:
     MOV R7, #4              @ write syscall, 4
     MOV R0, #1              @ output stream to monitor, 1
     MOV R2, #14             @ print string length
-    LDR R1, =prompt_str1    @ string at label prompt_str:
+    LDR R1, =prompt_str1    @ string at label prompt_str1:
     SWI 0                   @ execute syscall
     MOV PC, LR              @ return
 
@@ -41,7 +41,7 @@ _prompt2:
     MOV R7, #4              @ write syscall, 4
     MOV R0, #1              @ output stream to monitor, 1
     MOV R2, #11             @ print string length
-    LDR R1, =prompt_str2    @ string at label cprompt_str
+    LDR R1, =prompt_str2    @ string at label prompt_str2:
     SWI 0                   @ execute syscall
     MOV PC, LR              @ return
 
@@ -49,14 +49,14 @@ _prompt3:
     MOV R7, #4              @ write syscall, 4
     MOV R0, #1              @ output stream to monitor, 1
     MOV R2, #15             @ print string length
-    LDR R1, =prompt_str3    @ string at label prompt_str:
+    LDR R1, =prompt_str3    @ string at label prompt_str3:
     SWI 0                   @ execute syscall
     MOV PC, LR              @ return
 
 
 _ans:
     MOV R7, LR              @ store LR since printf call overwrites
-    LDR R0, =ans_str     @ R0 contains formatted string address
+    LDR R0, =ans_str        @ R0 contains formatted string address
     MOV R1, R1              @ R1 contains printf argument (redundant line)
     BL printf               @ call printf
     MOV PC, R7              @ return
@@ -65,11 +65,11 @@ _comp:
     CMP R2, #'+'            @ compare against the constant char '+'
     BEQ sum                 @ branch to equal handler
     CMP R2, #'-'            @ compare against the constant char '-'
-    BEQ diff          @ branch to equal handler
-    CMP R2, #'*'            @ compare against the constant char '-'
-    BEQ prod             @ branch to equal handler
-    CMP R2, #'M'            @ compare against the constant char '-'
-    BEQ max         @ branch to equal handler
+    BEQ diff                @ branch to equal handler
+    CMP R2, #'*'            @ compare against the constant char '*'
+    BEQ prod                @ branch to equal handler
+    CMP R2, #'M'            @ compare against the constant char 'M'
+    BEQ max                 @ branch to equal handler
     MOV PC, LR
 
 sum:
@@ -80,19 +80,19 @@ sum:
 diff:
     MOV R7, LR
     SUB R0, R1, R3          @ R1 - R3 = R0
-    MOV PC, R7
+    MOV PC, R7              @ return
 
 prod:
     MOV R7, LR
     MUL R0, R1, R3          @ R0 = R1*R3
-    MOV PC, R7
+    MOV PC, R7              @ return
 
 max:
     MOV R7, LR
     CMP R1, R3              @ compare R1, R3
     MOVLE R1, R3            @ overwrite R1 with R3 if R1 is lesser than or equal to R3
     MOV R0, R1              @ move the value from reg R1 to reg R0
-    MOV PC, R7
+    MOV PC, R7              @ return
 
 
 _scanf1:
@@ -110,7 +110,7 @@ _scanf2:
     MOV R7, #3              @ write syscall, 3
     MOV R0, #0              @ input stream from monitor, 0
     MOV R2, #1              @ read a single character
-    LDR R1, =char_str      @ store the character in data memory
+    LDR R1, =char_str       @ store the character in data memory
     SWI 0                   @ execute the system call
     LDR R0, [R1]            @ move the character to the return register
     AND R0, #0xFF           @ mask out all but the lowest 8 bits
