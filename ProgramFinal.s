bc35e29e05febcb07e57c656c1162d6bc9113c26
adhish9 <adhish.deshpande@gmail.com>
2016-05-08 21:16:31 -0500

    update final program

--- ProgramFinal.s
+++ ProgramFinal.s
@@ -18,61 +18,61 @@ _populate_array:
 	ADD R5 ,R0 ,R5
 	POP {R1}
 	POP {R0}
-    	LDR R1, =array_a              @ get address of a
-	LSL R2, R0, #2          @ multiply index*4 to get array offset
-    	ADD R2, R1, R2          @ R2 now has the element address
-	STR R4, [R2]            @ write the address of a[i] to a[i]
-	ADD R0, R0, #1          @ increment index
-    	B _populate_array           @ branch to next loop iteration
+    	LDR R1, =array_a              
+	LSL R2, R0, #2
+    	ADD R2, R1, R2      
+	STR R4, [R2]       
+	ADD R0, R0, #1         
+    	B _populate_array        
 _populate_array_done:
-    MOV R0, #0              @ initialze index variable
+	MOV R0, #0             
 _printf_array:	    	
 	CMP R0, #10       
     	BEQ _printf_array_done
     	LDR R1, =array_a
     	LSL R2, R0, #2
-    	ADD R2, R1, R2          @ R2 now has the element address
-	LDR R1, [R2]            @ read the array at address 
-    	PUSH {R0} 	             @ backup register before printf
-    	PUSH {R1}               @ backup register before printf
-    	PUSH {R2}               @ backup register before printf
-    	MOV R2,	R1              @ move array value to R2 for printf
-    	MOV R1, R0              @ move array index to R1 for printf
-    	BL  _printf             @ branch to print procedure with return
-    	POP {R2}                @ restore register
-	POP {R1}                @ restore register
-    	POP {R0}                @ restore register
-	ADD R0, R0, #1          @ increment index
-    	B   _printf_array            @ branch to next loop iteration
+    	ADD R2, R1, R2          
+	LDR R1, [R2]           
+    	PUSH {R0} 	         
+    	PUSH {R1}              
+    	PUSH {R2}             
+    	MOV R2,	R1             
+    	MOV R1, R0              
+    	BL  _printf             
+    	POP {R2}                
+	POP {R1}                
+    	POP {R0}                
+	ADD R0, R0, #1         
+    	B   _printf_array           
 _printf_array_done:
 	MOV R1, R5
-    	B _exit                 @ exit if done
+    	B _exit                 
 
 _scanf:
-    PUSH {LR}              @ store LR since scanf call overwrites
-    SUB SP, SP, #4          @ make room on stack
-    LDR R0, =format_str     @ R0 contains address of format string
-    MOV R1, SP              @ move SP to R1 to store entry on stack
-    BL scanf                @ call scanf
-    LDR R0, [SP]            @ load value at SP into R0
-    ADD SP, SP, #4          @ restore the stack pointer
+	PUSH {LR}             
+    	SUB SP, SP, #4         
+    	LDR R0, =format_str    
+    	MOV R1, SP             
+    	BL scanf               
+    	LDR R0, [SP]           
+    	ADD SP, SP, #4        
 	POP {LR}
 	MOV PC, LR
 
 _printf:
-    PUSH {LR}               @ store the return address
-    LDR R0, =printf_str     @ R0 contains formatted string address
-    BL printf               @ call printf
-    POP {PC}                @ restore the stack pointer and return
+    	PUSH {LR}               
+    	LDR R0, =printf_str     
+    	BL printf              
+    	POP {PC}                
     
 _exit:  
-    MOV R7, #4              @ write syscall, 4
-    MOV R0, #1              @ output stream to monitor, 1
-    MOV R2, #21             @ print string length
-    LDR R1, =exit_str       @ string at label exit_str:
-    SWI 0                   @ execute syscall
-    MOV R7, #1              @ terminate syscall, 1
-    SWI 0                   @ execute syscall
+	MOV R7, #4              
+    	MOV R0, #1             
+    	MOV R2, #21             
+	LDR R1, =exit_str       
+	SWI 0                   
+	MOV R7, #1             
+	SWI 0                  
 
 .data
 
